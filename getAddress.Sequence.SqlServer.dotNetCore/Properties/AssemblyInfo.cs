c885596ba41f83921fbf2e2ad7ef10016b5b4e12
geffzhang <geffzhang@qq.com>
2016-05-19 09:25:56 +0800

    modify namesapce

--- getAddress.Sequence.SqlServer.dotNetCore/Properties/AssemblyInfo.cs
+++ getAddress.Sequence.SqlServer.dotNetCore/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("getAddress.Sequence.SqlServer.dotNetCore")]
+[assembly: AssemblyProduct("getAddress.Sequence.SqlServer")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
--- getAddress.Sequence.dotNetCore/Properties/AssemblyInfo.cs
+++ getAddress.Sequence.dotNetCore/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("getAddress.Sequence.dotNetCore")]
+[assembly: AssemblyProduct("getAddress.Sequence")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
